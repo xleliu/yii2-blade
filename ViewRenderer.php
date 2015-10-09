4a26ae1a617e9d77f58fe2ee74a54f38e2375164
Scholer <scholer_l@live.com>
2015-10-09 23:02:10 +0800

    php-cs-fixer

--- ViewRenderer.php
+++ ViewRenderer.php
@@ -1,16 +1,14 @@
 <?php
+
 /**
  * Author : Scholer <scholer_l@live.com>
- * date   : 2015-10-04
+ * date   : 2015-10-04.
  */
 namespace xiaoler\blade;
 
 use Yii;
 use yii\web\View;
-use yii\base\Widget;
-use yii\base\InvalidConfigException;
 use yii\base\ViewRenderer as BaseViewRenderer;
-
 use Illuminate\Container\Container;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Filesystem\Filesystem;
@@ -24,37 +22,39 @@ use Illuminate\View\Factory as Blade;
 class ViewRenderer extends BaseViewRenderer
 {
     /**
-     * instance of Blade
+     * instance of Blade.
      *
      * @var Illuminate\View\Factory
      */
     protected $blade;
 
     /**
-     * extended extension of templete
+     * extended extension of templete.
      *
      * @var array
      */
     protected $extensions = ['bl' => 'blade'];
 
     /**
-     * blade complied cache path
+     * blade complied cache path.
      *
      * @var string
      */
     protected $cachePath = '@runtime/Blade/cache';
 
     /**
-     *  view file path, support yii aliases
+     *  view file path, support yii aliases.
      *
      * @var array
      */
-    protected $viewPath = ['@app/views', '@yii/views', '@vendor/yiisoft/yii2-debug/views'];
+    protected $viewPath = [
+        '@app/views',
+        '@yii/views',
+        '@vendor/yiisoft/yii2-debug/views',
+    ];
 
     /**
-     * initialize function, called by yii2
-     *
-     * @return null
+     * initialize function, called by yii2.
      */
     public function init()
     {
@@ -72,12 +72,13 @@ class ViewRenderer extends BaseViewRenderer
     }
 
     /**
-     * override function render of yii\base\ViewRenderer
+     * override function render of yii\base\ViewRenderer.
      *
-     * @param  yii\web\View $view
-     * @param  string $file  absolute file path of view
-     * @param  array $params params to view file
-     * @return string        complied templete
+     * @param yii\web\View $view
+     * @param string       $file   absolute file path of view
+     * @param array        $params params to view file
+     *
+     * @return string complied templete
      */
     public function render($view, $file, $params)
     {
@@ -94,14 +95,16 @@ class ViewRenderer extends BaseViewRenderer
 
         $params['app'] = \Yii::$app;
         $params['view'] = $view;
+
         return $this->blade->make($file, $params)->render();
     }
 
     /**
-     *  trim file extension of view
+     *  trim file extension of view.
      *
-     * @param  string $file relative file path of view
-     * @return string       relative file path of view without extension
+     * @param string $file relative file path of view
+     *
+     * @return string relative file path of view without extension
      */
     protected function trimExtension($file)
     {
@@ -111,21 +114,23 @@ class ViewRenderer extends BaseViewRenderer
             if ($diff == 0) {
                 return 0;
             }
+
             return $diff > 0 ? -1 : 1;
         });
         foreach ($extensions as $ext) {
-            $ext = '.' . $ext;
+            $ext = '.'.$ext;
             $length = strlen($ext);
             if (substr($file, -$length) === $ext) {
                 return substr_replace($file, '', -$length);
             }
         }
+
         return $file;
     }
 
     /**
      * get instance of Illuminate\Container\Container
-     * see [https://github.com/illuminate/view/blob/master/ViewServiceProvider.php]
+     * see [https://github.com/illuminate/view/blob/master/ViewServiceProvider.php].
      *
      * @return Illuminate\Container\Container
      */
@@ -136,13 +141,13 @@ class ViewRenderer extends BaseViewRenderer
             return Yii::getAlias($alias);
         }, $this->viewPath);
 
-        $container = new Container;
+        $container = new Container();
 
         $container->bindShared('files', function () {
-            return new Filesystem;
+            return new Filesystem();
         });
         $container->bindShared('events', function () {
-            return new Dispatcher;
+            return new Dispatcher();
         });
         $container->bindShared('view.finder', function ($app) use ($views) {
             return new FileViewFinder($app['files'], $views);
@@ -151,13 +156,14 @@ class ViewRenderer extends BaseViewRenderer
             return new BladeCompiler($app['files'], $cache);
         });
         $container->bindShared('view.engine.resolver', function () use ($cache, $container) {
-            $resolver = new EngineResolver;
+            $resolver = new EngineResolver();
             $resolver->register('php', function () {
-                return new PhpEngine;
+                return new PhpEngine();
             });
             $resolver->register('blade', function () use ($container) {
                 return new CompilerEngine($container['blade.compiler'], $container['files']);
             });
+
             return $resolver;
         });
 
